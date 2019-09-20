48a5a2feea7e2a32cdde7cae55de82df9695a4d2
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-09-19 22:43:24 -0400

    zomato api working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,19 @@
+// Find all bbq locations in Austin, TX
+const api_url = "https://developers.zomato.com/api/v2.1/search?q=bbq%2C%20barbecue&lat=40.7128&lon=-74.0060&radius=100000";
+
 $.ajax({
-          url: "https://developers.zomato.com/api/v2.1/search?q=" + 06901 + "&apikey=b4a3b5d555f0d53017007003d4445c87",
-          method: "GET"
-        }).done(function(response){
-          var results = response.data;
-        	// console.log(results);
+    url       : api_url,
+    method    : "GET",
+    dataType  : "JSON",
+    beforeSend: setHeader
+
+}).done(function(response) {
+    const restaurants = [];
+    console.log(response);
 
 
-        });
+});
+
+function setHeader(xhr) {
+    xhr.setRequestHeader('user-key', 'b4a3b5d555f0d53017007003d4445c87');
+}
