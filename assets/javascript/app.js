0566d56291e763cdff63320287d05dc83829c625
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-09-19 23:48:57 -0400

    movie api working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,3 +17,20 @@ $.ajax({
 function setHeader(xhr) {
     xhr.setRequestHeader('user-key', 'b4a3b5d555f0d53017007003d4445c87');
 }
+
+$.ajax({
+    url: "https://api-gate2.movieglu.com/filmsComingSoon/?n=10",
+    method: "GET",
+    dataType: "json",
+    headers: {
+        'api-version': "v200",
+        'authorization': "Basic VFlGVzo1Z201MXJZUGZnaGQ=",
+        'x-api-key': "0ZX6GGUcgC3troqj2GfJxq8AZtazYLO32vIqSkk7",
+        'device-datetime': "2018-11-27T13:26:30.147Z",
+        // 'geolocation': "" + userPosition + "",
+        'territory': "US",
+        'client': "BUSI"
+    }
+}).then(function(response) {
+    console.log(response)
+});
