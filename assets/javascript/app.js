d95c86c5d1db7e7380b9f70040e100822d4de1b9
Zachary Cohen <zacharycohen@Zacharys-MacBook-Pro.local>
2019-09-21 14:07:11 -0400

    api object ready to display to dom

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,40 @@
 
-const api_url = "https://developers.zomato.com/api/v2.1/search?q=bbq%2C%20barbecue&lat=40.7128&lon=-74.0060&radius=100000";
 
 $.ajax({
-    url       : api_url,
-    method    : "GET",
-    dataType  : "JSON",
-    beforeSend: setHeader
+  url: "http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-09-21&zip=06901&api_key=jx28yp3s6ukdynup5javhjmn",
+  method: "GET"
+}).done(handleMovies);
 
-}).done(function(response) {
-    const restaurants = [];
-    console.log(response);
+function handleMovies(response) {
+  var results = response;
+  results.forEach(getMovieInfo);
+  console.log(results);
 
 
-});
-
-function setHeader(xhr) {
-    xhr.setRequestHeader('user-key', 'b4a3b5d555f0d53017007003d4445c87');
 }
 
-$.ajax({
-    url: "https://api-gate2.movieglu.com/filmsComingSoon/?n=10",
-    method: "GET",
-    dataType: "json",
-    headers: {
-        'api-version': "v200",
-        'authorization': "Basic VFlGVzo1Z201MXJZUGZnaGQ=",
-        'x-api-key': "0ZX6GGUcgC3troqj2GfJxq8AZtazYLO32vIqSkk7",
-        'device-datetime': "2018-11-27T13:26:30.147Z",
-        // 'geolocation': "" + userPosition + "",
-        'territory': "US",
-        'client': "BUSI"
-    }
-}).then(function(response) {
-    console.log(response)
-});
+function getMovieInfo(movie) {
+  var myMovie = {};
+  myMovie.title = movie.title;
+  myMovie.genres = movie.genres[0];
+  myMovie.theater = movie.showtimes[0].theatre.name;
+
+  // var poster = results[i].preferredImage
+  // console.log(poster);
+  myMovie.rating = '';
+  if (movie.ratings) {
+    myMovie.rating = movie.ratings[0].code;
+  }
+
+  $.ajax({
+    url: "http://www.omdbapi.com/?t=" + encodeURI(movie.title) + "&apikey=698e080b",
+    method: "GET"
+  }).done(function(resp) {
+    var res = resp;
+    myMovie.poster = res.Poster;
+    // console.log(results);
+    console.log(myMovie);
+    // add movie to DOM
+  });
+
+}
