84f9d9e9fb4738ec04d951cc12b19c2bc4b7c194
Nick Frattaroli <Nafratt@gmail.com>
2019-09-21 10:53:13 -0400

    adding new html for wireframe

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,1 @@
-var isGameStarted = false;
-function startbutton() {
-    if (isGameStarted) {
-      return;
-    }
-  
-    isGameStarted = true;
-    var x = document.getElementById("useage");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+
