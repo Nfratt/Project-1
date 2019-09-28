b47f7c12144d909d7ff8d0a8b6fe61e5f25a3f5a
Nick Frattaroli <Nafratt@gmail.com>
2019-09-28 10:34:08 -0400

    fixing buttons and getting rid of more suggestions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,208 +1,278 @@
 $(document).ready(function () {
 
-  var location = 'Seymour,Connecticut';  //change to user input once identified
-  var city = 'Stamford'
-  var state = 'CT'
-  var date;
-
-
-  function displayResultsWeather() {
-      var APIKey = "166a433c57516f51dfab1f7edaed8413";
-
-      // Here we are building the URL we need to query the database
-      var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-          "q=" + location + "&units=imperial&appid=" + APIKey;
-
-      // Here we run our AJAX call to the OpenWeatherMap API
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-      }).then(function (response) {
-
-          // Log the resulting object
-          //console.log(response);
-          console.log('-----WEATHER-----')
-
-          var windMPH = Math.floor(response.wind.speed * 2.237);
-          var tempF = Math.floor(response.main.temp);
-
-
-          // Transfer content to HTML
-          //  $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-          //  $(".wind").text("Wind Speed: " + response.wind.speed);
-          //  $(".humidity").text("Humidity: " + response.main.humidity);
-          //  $(".temp").text("Temperature (F) " + response.main.temp);
-
-          // Log the data in the console as well
-          console.log("Temperature (F): " + tempF);
-          console.log("Wind Speed: " + windMPH);
-          console.log("Humidity: " + response.main.humidity + ' %');
-
-          var weatherDiv = $('<div>');
-          var pTemp = $('<p>').append("Temperature: " + tempF + 'F');
-          var pWind = $('<p>').append("Wind Speed: " + windMPH + 'mph');
-          var pHumidity = $('<p>').append("Humidity: " + response.main.humidity + '%');
-
-          weatherDiv.append(pTemp);
-          weatherDiv.append(pWind);
-          weatherDiv.append(pHumidity);
-          $('#weather').append(weatherDiv);
-
-      });
-
-  };
-  displayResultsWeather();
-
-  function displayResultsEvents() {
-      // Here we are building the URL we need to query the database
-      var queryURL = 'https://app.ticketmaster.com/discovery/v2/events?apikey=R6DhROIJxrsIGyZyaj1qzfCpwqb7IsA9&locale=*&&city=' + city + '&stateCode=' + state
-
-      // Here we run our AJAX call to the OpenWeatherMap API
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-      }).then(function (response) {
-          var events = response._embedded.events;
-          console.log('-----EVENTS-----')
-          for (var i = 0; i < events.length; i++) {
-              console.log(events[i].name);
-              console.log(events[i].dates.start.localDate);
-              try {
-                  console.log(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
-              } catch (err) {
-                  console.log(err);
+    var urlParams = new URLSearchParams(window.location.search);
+      console.log(urlParams.get("location"));
+      var location = urlParams.has("location") ? urlParams.get("location"): "";
+  
+      // var location = 'Seymour,Connecticut'; //change to user input once identified
+      var city = urlParams.has("city")? urlParams.get("location"): "";
+      var state = urlParams.has("state")? urlParams.get("state"): "";
+      var date = urlParams.has("date")? urlParams.get("date"): "";
+      var startdate = urlParams.has("date")? urlParams.get("date"): "";
+      var enddate = urlParams.has("date")? urlParams.get("date"): "";
+      var zip = urlParams.has("zip")? urlParams.get("zip"): "";
+      $("#startbtn").on("click", function(event) {
+          event.preventDefault();
+  
+          // console.log("clicked");
+          // handleMovies(response);
+          // moviePass();
+  
+          // console.log(zip);
+  
+          location = $("#inputLocationCity").val().trim();
+          city = location;
+          state = $("#inputLocationState").val().trim();
+          startdate = $("#inputDate").val().trim();
+          zip = $('#zip-code').val().trim();
+          // displayResultsWeather(location, city, state, date, zip);
+          // displayResultsEvents(location,city,state,date,zip);
+          // displayResultsFood(location,city,state,date,zip);
+          // handleMovies(location,city,state,date,zip);
+          // getMovieInfo(location,city,state,date,zip);
+  
+          // Load the next page
+          window.location.href = `D8nite.html?location=${location}&city=${city}&zip=${zip}&date=${date}&state=${state}`;
+  
+          // state = $("inputLocationState").val().trim();
+          // displayResultsEvents();
+          // localStorage.clear();
+          //
+          // // Store all content into localStorage
+          // localStorage.setItem("location", location);
+          // localStorage.setItem("state", state);
+  
+  
+        });
+  
+  
+      function displayResultsWeather() {
+          var APIKey = "166a433c57516f51dfab1f7edaed8413";
+  
+          // Here we are building the URL we need to query the database
+          var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+              "q=" + location + "&units=imperial&appid=" + APIKey;
+  
+          // Here we run our AJAX call to the OpenWeatherMap API
+          $.ajax({
+              url: queryURL,
+              method: "GET"
+          }).then(function (response) {
+  
+              // Log the resulting object
+              //console.log(response);
+              console.log('-----WEATHER-----')
+  
+              var windMPH = Math.floor(response.wind.speed * 2.237);
+              var tempF = Math.floor(response.main.temp);
+  
+              // Log the data in the console as well
+              console.log("Temperature (F): " + tempF);
+              console.log("Wind Speed: " + windMPH);
+              console.log("Humidity: " + response.main.humidity + ' %');
+  
+              var weatherDiv = $('<div>');
+              var pTemp = $('<p>').append("Temperature: " + tempF + 'F');
+              var pWind = $('<p>').append("Wind Speed: " + windMPH + 'mph');
+              var pHumidity = $('<p>').append("Humidity: " + response.main.humidity + '%');
+  
+  
+              weatherDiv.append(pTemp);
+              weatherDiv.append(pWind);
+              weatherDiv.append(pHumidity);
+              $('#weatherResults').append(weatherDiv);
+  
+          });
+  
+      };
+      displayResultsWeather();
+  
+      function displayResultsEvents() {
+          // Here we are building the URL we need to query the database
+          var queryURL = 'https://app.ticketmaster.com/discovery/v2/events?apikey=R6DhROIJxrsIGyZyaj1qzfCpwqb7IsA9&locale=*&city=' + city + '&stateCode=' + state + '&startDateTime=' + startdate + '&endDateTime=' + enddate
+  
+          // Here we run our AJAX call to the OpenWeatherMap API
+          $.ajax({
+              url: queryURL,
+              method: "GET"
+          }).then(function (response) {
+              var events = response._embedded.events;
+              console.log('-----EVENTS-----')
+  
+              for (var i = 0; i < events.length; i++) {
+                  console.log(events[i].name);
+                  console.log(events[i].dates.start.localDate);
+                  try {
+                      console.log(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
+                  } catch (err) {
+                      console.log(err);
+                  }
+                  console.log(events[i].url);
+  
+                  //   var eventDiv = $('<div>');
+                  //   var pEventName = $('<p>').append(events[i].name);
+                  //   var pEventDate = $('<p>').append(events[i].dates.start.localDate);
+                  //   var pEventVenue = $('<p>').append(events[i]._embedded.venues[0].name);
+                  //   var pEventCity = $('<p>').append(events[i]._embedded.venues[0].city.name);
+                  //   var pEventTickets = $('<p>').append(events[i].url);
+  
+                  var eventCard = $('<div>').addClass('card');
+                  var pEventName = $('<h5>').addClass('card-title');
+                  pEventName.append(events[i].name);
+  
+                  var pEventDate = $('<h6>').addClass('card-text')
+                  pEventDate.append(events[i].dates.start.localDate);
+  
+  
+                  var pEventVenue = $('<p>').addClass('card-text');
+                  pEventVenue.append(events[i]._embedded.venues[0].name + ' , ' + events[i]._embedded.venues[0].city.name);
+  
+                  var pEventTickets = $('<a href=' + events[i].url + '>').addClass('btnbook btn-primary').text('Get Tickets')
+  
+  
+                  eventCard.append(pEventName);
+                  eventCard.append(pEventDate);
+                  eventCard.append(pEventVenue);
+                  eventCard.append(pEventTickets);
+                  // eventDiv.append($('<hr>'));
+                  $('#eventResults').append(eventCard);
               }
-              console.log(events[i].url);
-
-              var eventDiv = $('<div>');
-              var pEventName = $('<p>').append(events[i].name);
-              var pEventDate = $('<p>').append(events[i].dates.start.localDate);
-              var pEventVenue = $('<p>').append(events[i]._embedded.venues[0].name);
-              var pEventCity = $('<p>').append(events[i]._embedded.venues[0].city.name);
-              var pEventTickets = $('<p>').append(events[i].url);
-
-              eventDiv.append(pEventName);
-              eventDiv.append(pEventDate);
-              eventDiv.append(pEventVenue);
-              eventDiv.append(pEventCity);
-              eventDiv.append(pEventTickets);
-              eventDiv.append($('<hr>'));
-              $('#eventResults').append(eventDiv);
-          }
-      });
-  };
-  ;
-  displayResultsEvents();
-
-
-  function displayResultsFood() {
-
-      var queryURL = 'https://opentable.herokuapp.com/api/restaurants?city=' + city + '&state=' + state;
-
-      // Here we run our AJAX call to the OpenWeatherMap API
+  
+  
+  
+              // } else {
+              //     var eventCard = $('<div>').addClass('card');
+              //     var error = $('<h3>').addClass('card-title');
+              //     error.text('Sorry, no events on this day. Try moving your date to another day.');
+              //     eventCard.append(error);
+              //     $('#eventResults').append(eventCard);
+              // };
+  
+  
+  
+          });
+      };
+  
+      displayResultsEvents();
+  
+  
+      function displayResultsFood() {
+  
+          var queryURL = 'https://opentable.herokuapp.com/api/restaurants?city=' + city + '&state=' + state + '&per_page=5';
+  
+          // Here we run our AJAX call to the OpenWeatherMap API
+          $.ajax({
+              url: queryURL,
+              method: 'GET'
+          }).then(function (response) {
+              console.log('-----RESTAURANTS-----')
+  
+              var restaurants = response.restaurants;
+              for (var i = 0; i < restaurants.length; i++) {
+                  console.log(response.restaurants[i].image_url);
+                  console.log(response.restaurants[i].name);
+                  console.log(response.restaurants[i].address);
+                  console.log(response.restaurants[i].city);
+                  console.log(response.restaurants[i].phone);
+                  console.log(response.restaurants[i].reserve_url);
+  
+  
+  
+  
+                  var restCard = $('<div>').addClass('card');
+                  var pRestName = $('<h5>').addClass('card-title');
+                  pRestName.append(response.restaurants[i].name);
+  
+                  var pRestAddress = $('<h6>').addClass('card-text')
+                  pRestAddress.append(response.restaurants[i].address + ' , ' + response.restaurants[i].city);
+  
+                  var pRestPhone = $('<p>').addClass('card-text');
+                  pRestPhone.append(response.restaurants[i].phone);
+  
+                  var pRestReserve = $('<a href=' + response.restaurants[i].reserve_url + '>').addClass('btnbook').attr('id', 'reserveBtn').text('Reserve Now')
+  
+                  restCard.append(pRestName);
+                  restCard.append(pRestAddress);
+                  //restCard.append(pRestCity);
+                  restCard.append(pRestPhone);
+                  restCard.append(pRestReserve);
+                  $('#restResults').append(restCard);
+              };
+          });
+      };
+  
+      displayResultsFood();
+  
+  
+  
+  
       $.ajax({
-          url: queryURL,
-          method: 'GET'
-      }).then(function (response) {
-          console.log('-----RESTAURANTS-----')
-
-          var restaurants = response.restaurants;
-          for (var i = 0; i < restaurants.length; i++) {
-              console.log(response.restaurants[i].image_url);
-              console.log(response.restaurants[i].name);
-              console.log(response.restaurants[i].address);
-              console.log(response.restaurants[i].city);
-              console.log(response.restaurants[i].phone);
-              console.log(response.restaurants[i].reserve_url);
-
-              var restDiv = $('<div>');
-              var pRestName = $('<p>').append(response.restaurants[i].name);
-              var pRestAddress = $('<p>').append(response.restaurants[i].address);
-              var pRestCity = $('<p>').append(response.restaurants[i].city);
-              var pRestPhone = $('<p>').append(response.restaurants[i].phone);
-              var pRestReserve = $('<p>').append(response.restaurants[i].reserve_url);
-
-              restDiv.append(pRestName);
-              restDiv.append(pRestAddress);
-              restDiv.append(pRestCity);
-              restDiv.append(pRestPhone);
-              restDiv.append(pRestReserve);
-              restDiv.append($('<hr>'));
-              $('#restResults').append(restDiv);
-          };
-      });
-  };
-
-  displayResultsFood();
-
-
-
-
-  $.ajax({
-      url: "http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-09-25&zip=06901&api_key=7hx5n3fk8fejujqvtd3xxcpr",
-      method: "GET"
-  }).done(handleMovies);
-
-  function handleMovies(response) {
-      var results = response;
-      results.forEach(getMovieInfo);
-      console.log(results);
-  }
-
-  function getMovieInfo(movie) {
-      var myMovie = {};
-      myMovie.title = movie.title;
-      myMovie.genres = movie.genres[0];
-      myMovie.theater = movie.showtimes[0].theatre.name;
-      myMovie.fandango = movie.showtimes[0].ticketURI
-
-      // var poster = results[i].preferredImage
-      // console.log(poster);
-      myMovie.rating = '';
-      if (movie.ratings) {
-          myMovie.rating = movie.ratings[0].code;
-      }
-
-      $.ajax({
-          url: "http://www.omdbapi.com/?t=" + encodeURI(movie.title) + "&apikey=698e080b",
+          url: "http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-09-25&zip="+zip+"&api_key=7hx5n3fk8fejujqvtd3xxcpr",
           method: "GET"
-      }).done(function (resp) {
-          var res = resp;
-          myMovie.poster = res.Poster;
-          // console.log(results);
-          console.log(myMovie);
-          // add movie to DOM
-          var movieDiv = $('<div>');
-            var movieName = $('<p>').append(myMovie.title);
-            var movieGenre = $('<p>').append(myMovie.genres);
-            var movieTheater = $('<p>').append(myMovie.theater);
-            var movieDango = $('<a href='+myMovie.fandango+'>').append(myMovie.fandango);
-            $(movieDango).html("Buy Now ");
-
-            var movieRating = $('<p>').append(myMovie.rating);
-            var showImage = $("<img>");
+      }).done(handleMovies);
+  
+      function handleMovies(response) {
+          var results = response;
+          results.forEach(getMovieInfo);
+          console.log(results);
+      }
+  
+      function getMovieInfo(movie) {
+          var myMovie = {};
+          myMovie.title = movie.title;
+          myMovie.genres = movie.genres[0];
+          myMovie.theater = movie.showtimes[0].theatre.name;
+          myMovie.fandango = movie.showtimes[0].ticketURI
+  
+          // var poster = results[i].preferredImage
+          // console.log(poster);
+          myMovie.rating = '';
+          if (movie.ratings) {
+              myMovie.rating = movie.ratings[0].code;
+          }
+  
+          $.ajax({
+              url: "http://www.omdbapi.com/?t=" + encodeURI(movie.title) + "&apikey=698e080b",
+              method: "GET"
+          }).done(function (resp) {
+              var res = resp;
+              myMovie.poster = res.Poster;
+              // console.log(results);
+              console.log(myMovie);
+              // add movie to DOM
+              var movieDiv = $('<div>').addClass('card');
+              var movieName = $('<h5>').addClass('card-title')
+              movieName.append(myMovie.title);
+              var movieGenre = $('<h6>').addClass('card-text')
+              movieGenre.append(myMovie.genres);
+              var movieTheater = $('<p>').addClass('card-text')
+              movieTheater.append(myMovie.theater);
+              var movieDango = $('<a href=' + myMovie.fandango + '>').addClass('btn btn-primary').attr('id','dangoBtn').text('Buy Now');
+  
+  
+              var movieRating = $('<p>').append(myMovie.rating);
+              var showImage = $("<img>");
               showImage.attr("src", myMovie.poster)
               // movieDango.attr("href",myMovie.fandango)
-
-
-            movieDiv.append(movieName);
-            movieDiv.append(movieGenre);
-            movieDiv.append(movieDango);
-            movieDiv.append(movieTheater);
-            movieDiv.append(movieRating);
-
-            movieDiv.append(showImage);
-
-            movieDiv.append($('<hr>'));
-            $('#movieResults').append(movieDiv);
-      });
-
-  };
-
-});
-
-// On click event for start button
-// target the inputs and look at the values
-// zipcode use dot val dot trim
-// variable for the zip code on click zipcode eq zip id val trim
-// function when the api gets called
+  
+  
+              movieDiv.append(movieName);
+              movieDiv.append(movieGenre);
+  
+              movieDiv.append(movieTheater);
+              movieDiv.append(movieRating);
+  
+              movieDiv.append(showImage);
+  
+              movieDiv.append(movieDango);
+              $('#movieResults').append(movieDiv);
+          });
+  
+      };
+    displayResultsWeather();
+    displayResultsFood();
+    displayResultsEvents();
+    handleMovies();
+  
+  });
+  
